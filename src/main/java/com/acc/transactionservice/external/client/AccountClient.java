7a956fccf9bc66def3e5e92c851c433fc183e966
latif <latifirfansyah@gmail.com>
2024-09-12 13:50:11 +0700

    fix feign client

--- src/main/java/com/acc/transactionservice/external/client/AccountClient.java
+++ src/main/java/com/acc/transactionservice/external/client/AccountClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "account-management-service", url = "/api/v1/accounts")
+@FeignClient(name = "account-management-service/api/v1/accounts")
 public interface AccountClient {
 
     @GetMapping("/{id}")
